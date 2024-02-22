fb6f58d21afdd600e047366f977d2b9a409e4fbe
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2024-02-22 13:08:39 +0300

    Add Log4j, implement simple test

--- marchingcubes/app/src/test/java/ru/duckcoder/marchingcubes/mapgenerator/ApplicationTest.java
+++ marchingcubes/app/src/test/java/ru/duckcoder/marchingcubes/mapgenerator/ApplicationTest.java
@@ -3,12 +3,35 @@
  */
 package ru.duckcoder.marchingcubes.mapgenerator;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class ApplicationTest {
-    @Test void appHasAGreeting() {
-        Application classUnderTest = new Application();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InaccessibleObjectException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@Log4j2
+public class ApplicationTest {
+    @Test
+    public void mainMethodTest(){
+        Method[] declaredMethods = Application.class.getDeclaredMethods();
+        PrintStream tmp = System.out;
+        System.setOut(new PrintStream(new ByteArrayOutputStream()));
+        try {
+            Arrays.stream(declaredMethods)
+                    .peek(method -> method.setAccessible(true))
+                    .filter(method -> method.getName().equals("main")
+                            && method.getReturnType().equals(Void.TYPE))
+                    .forEach(method -> assertDoesNotThrow(() -> {
+                        method.invoke(null, (Object) new String[]{});
+                    }));
+        } catch (SecurityException | InaccessibleObjectException e) {
+            log.error(e.getMessage());
+        }
+        System.setOut(tmp);
     }
 }
